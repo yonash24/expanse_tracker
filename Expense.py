fa150838b7c64a82a0d21f36591b7053ff75d318
yonash24 <107315024+yonash24@users.noreply.github.com>
2024-10-22 23:20:40 +0300

    create all the methods in Expense

--- Expense.py
+++ Expense.py
@@ -1,11 +1,9 @@
-from abc import ABC, abstractmethod
 import datetime
-from unicodedata import category
-
 import mysql.connector
 
-class Expense(ABC):
 
+
+class Expense:
     def __init__(self, amount, description, category, extra_info):
         self.date =  datetime.datetime.now()
         self.amount = amount
@@ -13,19 +11,65 @@ class Expense(ABC):
         self.category = category
         self.extra_info = extra_info
 
-    @abstractmethod
+    def connect_database(self):
+       return mysql.connector.connect(
+           host='localhost',
+           user='root',
+           database='expense_tracker'
+       )
+
     def add_to_database(self):
-        pass
+            connection = self.connect_database() #establish connection to db
+            cursor = connection.cursor()   #create cursor object
+            query = '''
+                INSERT INTO expenses(amount, description category, extra_info ,date)
+                VALUES(%S, $s, $s, $s, $s)
+            '''
+            values = (self.amount, self.description, self.category, self.extra_info, self.date) #the values we sign for each parameter at the quey
+            cursor.execute(query,values)    #execute in mysql query with the values
+            connection.commit()     #commit the transaction
+            cursor.close()      # close the cursor
+            connection.close()      #close the connection
 
-    @abstractmethod
-    def remove_from_database(self):
-        pass
+    def remove_from_database(self, expense_id):
+            connection = self.connect_database()
+            cursor = connection.cursor()
+            query = '''
+                DELETE FROM expenses
+                WHERE id = %s;
+            '''
+            cursor.execute(query,expense_id)
+            cursor.close()
+            connection.close()
 
-    @abstractmethod
-    def update_in_database(self):
-        pass
+    def update_in_database(self, expense_id):
+            connection = self.connect_database()
+            cursor = connection.cursor()
+            query = '''
+                UPDATE expenses 
+                SET date = $s, amount = %s, description = %s, category = $s, extra_info = %s
+                WHERE id = $s
+            '''
+            values = (self.date, self.amount, self.description, self.category, self.extra_info, expense_id)
+            cursor.execute(query,values)
+            cursor.close()
+            connection.close()
 
-    @abstractmethod
     def display_details(self):
-        pass
+            return f"Date: {self.date}, Amount:{self.amount}, Description:{self.description}, Category:{self.category}, Extra Info:{self.extra_info}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- data.sql
+++ data.sql
@@ -3,9 +3,9 @@ CREATE DATABASE expense_tracker;
 USE expense_tracker;
 
 CREATE TABLE expenses(
-        id INT,
+        id INT AUTO INCREMENT PRIMARY KEY,
         amount INT,
-        description VARCHAR(1000),
+        description VARCHAR(255),
         category VARCHAR(50),
         extra_info VARCHAR(50)
 )
